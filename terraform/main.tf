d02059b703cb2ac3dc71aaeaae816a9933bbeda7
Vishal_Kashyap <vv9026472@gmail.com>
2024-08-08 17:04:30 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,13 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your preferred region
+region = "us-east-1"  # Change to your preferred region
 }
 
 resource "aws_instance" "instance1" {
-  ami           = "ami-0b72821e2f351e396"  # Replace with a valid AMI ID for your region
-  instance_type = "t2.micro"
+ami           = "ami-0b72821e2f351e396"  # Replace with a valid AMI ID for your region
+instance_type = "t2.micro"
 tags = {
-    Name = "instance1-inst"
-  }
-
+  Name = "instance1-inst"
+ }
 }
 
 output "instance_id" {
