3aacb0bb9318ef3fe545b107569c7a4325155faf
Vishal-Kashap <vv9026472@gmail.com>
2024-08-09 05:17:36 +0000

    Apply terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "us-east-1"  # Change to your preferred region
+  region = "us-east-1" # Change to your preferred region
 }
 
 resource "aws_instance" "instance1" {
-ami           = "ami-04a81a99f5ec58529"  # Replace with a valid AMI ID for your region
-instance_type = "t2.micro"
-tags = { Name = "instance1-inst"
- }
+  ami           = "ami-04a81a99f5ec58529" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+  tags = { Name = "instance1-inst"
+  }
 }
 
 output "instance_id" {
-value = aws_instance.instance1.id
+  value = aws_instance.instance1.id
 }
 
 output "instance_public_ip" {
-value = aws_instance.instance1.public_ip
+  value = aws_instance.instance1.public_ip
 }
